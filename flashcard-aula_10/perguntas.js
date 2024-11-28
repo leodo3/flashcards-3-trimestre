a05e98825407f8300811aecfec272fe1ba55f731
leodo3 <166733077+leodo3@users.noreply.github.com>
2024-11-28 12:05:44 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'escola',
+    'em que ano o etelvina virou civico militar?',
+    '2021)'
+)
+
+criaCartao(
+    'Ed.física',
+    'de quantoa quanto tempo acontece as olimpiedas',
+    '4 anos '
+)
